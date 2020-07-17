38f30a5641a3131b0c477a6e0c29c1345cd3cfb6
N08I40K <55545594+N08I40K@users.noreply.github.com>
2020-07-17 16:36:03 +0400

    Timings update
    Моментальная загрузка)

--- main.py
+++ main.py
@@ -38,41 +38,40 @@ def main():
     print("init1")
     try:
         print("init2")
-        for event in long_poll.listen():
-            print("init3")
-            try:
-                print("init4")
-                script_path = os.path.abspath(__file__).replace("main.py", "")
-                configg_lines = open(script_path + "status.txt", encoding="utf-8").readlines()
-                def count_lines(filename, chunk_size=1<<13):
-                    with open(filename) as file:
-                        return sum(chunk.count('\n')
-                            for chunk in iter(lambda: file.read(chunk_size), ''))
-                count_lines("status.txt")
-                strnum = 0
-                scriptcycless = 0
-                strings = count_lines("status.txt") + 1
-                status_text = ""
-                while True:
-                    if strnum >= strings:
-                        strnum = 0
-                        scriptcycless += 1 
-                    # VARS
-                    delta = datetime.timedelta(hours=3, minutes=0) # разница от UTC. Можете вписать любое значение вместо 3
-                    t = (datetime.datetime.now(datetime.timezone.utc) + delta) # Присваиваем дату и время переменной «t»
-                    nowtime = t.strftime("%H:%M")
-                    nowdate = t.strftime("%d.%m.%Y")
-                    on = vk.friends.getOnline()
-                    friendsonlinee = len(on)
-                    # VARS                    
-                    status_text = configg_lines[int(strnum)].replace("scriptcycles", str(scriptcycless)).replace("friendsonline", str(friendsonlinee)).replace("currdate", str(nowdate)).replace("currtime", str(nowtime)).replace("randomeasyy", str(random.randint(-1000000000000000000000000000, 1000000000000000000000000000)))
-                    vk.status.set(text=status_text, random_id=random.randint(-2147483648, 2147483647))
-                    print("Ваш статус: " + status_text)
-                    strnum += 1
-                    time.sleep(int(timeout_set))
-            except Exception as message_error:
-                console_log("Ошибка")
-                continue
+        event = long_poll.listen()
+        print("init3")
+        try:
+            print("init4")
+            script_path = os.path.abspath(__file__).replace("main.py", "")
+            configg_lines = open(script_path + "status.txt", encoding="utf-8").readlines()
+            def count_lines(filename, chunk_size=1<<13):
+                with open(filename) as file:
+                    return sum(chunk.count('\n')
+                        for chunk in iter(lambda: file.read(chunk_size), ''))
+            count_lines("status.txt")
+            strnum = 0
+            scriptcycless = 0
+            strings = count_lines("status.txt") + 1
+            status_text = ""
+            while True:
+                if strnum >= strings:
+                    strnum = 0
+                    scriptcycless += 1 
+                # VARS
+                delta = datetime.timedelta(hours=3, minutes=0) # разница от UTC. Можете вписать любое значение вместо 3
+                t = (datetime.datetime.now(datetime.timezone.utc) + delta) # Присваиваем дату и время переменной «t»
+                nowtime = t.strftime("%H:%M")
+                nowdate = t.strftime("%d.%m.%Y")
+                on = vk.friends.getOnline()
+                friendsonlinee = len(on)
+                # VARS                    
+                status_text = configg_lines[int(strnum)].replace("scriptcycles", str(scriptcycless)).replace("friendsonline", str(friendsonlinee)).replace("currdate", str(nowdate)).replace("currtime", str(nowtime)).replace("randomeasyy", str(random.randint(-1000000000000000000000000000, 1000000000000000000000000000)))
+                vk.status.set(text=status_text, random_id=random.randint(-2147483648, 2147483647))
+                print("Ваш статус: " + status_text)
+                strnum += 1
+                time.sleep(int(timeout_set))
+        except Exception as message_error:
+            console_log("Ошибка")
 
     except Exception as vk_error:
         console_log("Ошибка")
